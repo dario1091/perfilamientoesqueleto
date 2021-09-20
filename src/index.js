8d204bc7ec3101f8eedc3be5f2855cefdeee5155
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-09-20 16:16:36 -0500

    Generar archivos .json automatizado

--- src/index.js
+++ src/index.js
@@ -3,19 +3,41 @@ const fs = require("fs");
 
 async function worker() {
   try {
-    //let awsPathFile="https://archivosavanzo.s3.us-east-2.amazonaws.com/1116446441/36/documentFront.png";
-    let awsPathFile = "src/docs/paymentSupport-e0d86fabde5a4c1d.jpg";
-    await readPaymentgSupport(awsPathFile).then((response) => {
-      if (response) {
-        console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
-        console.log(":::::::::::::::    response    ::::::::::::::::::");
-        console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
-        // console.log(response)
-        // response.map((x) => console.log(x));
-        console.log(JSON.parse(JSON.stringify(response)));
-        console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
+    // GENERAR .JSON AUTOMATIZADOS
+    await fs.readdir("src/docs", (err, files) => {
+      if (err) {
+        console.log("No existe el directorio 'docs'");
+        return err;
       }
+      files.map((f) => {
+        let nameFile = f.split(".")[0];
+        // console.log(nameFile);
+
+        readPaymentgSupport(`src/docs/${f}`).then((response) => {
+          if (response) {
+            console.log(
+              ":::::::::::::::    file generated    ::::::::::::::::::"
+            );
+            fs.writeFileSync(`json/${nameFile}.json`, JSON.stringify(response));
+          }
+        });
+      });
     });
+
+    // ----- AWS PATH FILE
+    // //let awsPathFile="https://archivosavanzo.s3.us-east-2.amazonaws.com/1116446441/36/documentFront.png";
+    // let awsPathFile = "src/docs/paymentSupport-e0d86fabde5a4c1d.jpg";
+    // await readPaymentgSupport(awsPathFile).then((response) => {
+    //   if (response) {
+    //     console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
+    //     console.log(":::::::::::::::    response    ::::::::::::::::::");
+    //     console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
+    //     // console.log(response)
+    //     // response.map((x) => console.log(x));
+    //     console.log(JSON.parse(JSON.stringify(response)));
+    //     console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
+    //   }
+    // });
   } catch (error) {
     console.log(error);
   }
