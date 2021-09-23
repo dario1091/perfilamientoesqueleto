3b1c85d24c99f28d7cf56401be953f89aa142cf8
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-09-23 15:24:14 -0500

    Lectura de datos estáticos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { readPaymentgSupport } = require("./utils/companies/digitex.js");
-const fs = require("fs");
+const { readPaymentgSupport } = require("./utils/companies/contactamos.js");
+// const fs = require("fs");
 
 async function worker() {
   try {
@@ -30,7 +30,7 @@ async function worker() {
 
     // ----- AWS PATH FILE
     // let awsPathFile="https://archivosavanzo.s3.us-east-2.amazonaws.com/1116446441/36/documentFront.png";
-    let awsPathFile = "src/docs/digitex/2/30570-1-1030556844-20200804.jpg";
+    let awsPathFile = "src/docs/contactamos/1/paymentSupport (1) 1.jpg";
     await readPaymentgSupport(awsPathFile).then((response) => {
       if (response) {
         console.log(":::::::::::::::::::::::::::::::::::::::::::::::::");
--- src/utils/companies/contactamos.js
+++ src/utils/companies/contactamos.js
@@ -32,13 +32,13 @@ const readPaymentgSupport = (filePath, isRequest = false) =>
           name: "",
           account: "",
         },
-        cargo: "",
-        salud: "",
+        cargo: "NO REGISTRA",
+        salud: "NO REGISTRA",
         basico: "",
         nomina: "",
-        pension: "",
+        pension: "NO REGISTRA",
         convenio: "",
-        fechaIngreso: "",
+        // fechaIngreso: "",
         devengos: {
           list: [],
           subtotal: null,
@@ -72,6 +72,8 @@ const readPaymentgSupport = (filePath, isRequest = false) =>
               if (block.BlockType === "LINE") {
                 let strTop =
                   block.Geometry.BoundingBox.Top.toString().substring(0, 6);
+
+                // console.log(block);
 
                 let pos = -1;
                 for (let index = 0; index < arrayTextLine.length; index++) {
@@ -109,35 +111,59 @@ const readPaymentgSupport = (filePath, isRequest = false) =>
           }
 
           /**
-           * Posición inicial de tabla de devengos
+           * Posición inicial de tabla de dev/ded
            */
-          let initDevengos = arrayTextLine
+          let init = arrayTextLine
             .map((e) => {
               return e.arrayText[0].text;
             })
             .indexOf("CONCEP DESCRIPCION CONCEPTO");
 
-          console.log(initDevengos);
+          // console.log(init);
 
           /**
-           * Posición inicial de tabla de deducciones
+           * Posición final de tabla de dev/ded y campo
+           * de subtotales devengos y deducciones
            */
-          let initDeducciones = arrayTextLine
-            .map((e) => {
-              return e.arrayText[0].text;
-            })
-            .indexOf("CENTRO COSTO CONCEP DESCRIPCIÓN CONCEPTO");
+          let end =
+            arrayTextLine
+              .map((e) => {
+                return e.arrayText[0].text;
+              })
+              .indexOf("Firma:") - 3;
 
-          console.log(initDeducciones);
+          // console.log(end);
+
+          // GUARDANDO REFERENCIAS DE SUBTOTALES Y LEFTS DE DEVENGOS Y DEDUCCIONES
 
           /**
-           * Posición final de tabla de devengos / descuentos
+           * Referencia del bloque donde se encuentran los subtotales
            */
-          //   let end = arrayTextLine
-          //     .map((e) => {
-          //       return e.arrayText[0].text;
-          //     })
-          //     .indexOf("Unidad Org.");
+          let bloque = 0;
+          // Subtotal de devengos y descuentos(deducciones)
+          if (arrayTextLine[end].arrayText[0]?.text.includes("NETO")) {
+            bloque = end - 1;
+
+            // EL BLOQUE TIENE NETO
+
+            leftEarns = arrayTextLine[bloque].arrayText[0]?.left;
+            leftDiscounts = arrayTextLine[bloque].arrayText[1]?.left;
+
+            jsonClient.devengos.subtotal =
+              arrayTextLine[bloque].arrayText[0]?.text.split("$ ")[1];
+            jsonClient.descuentos.subtotal =
+              arrayTextLine[bloque].arrayText[1]?.text.split("$ ")[1];
+          } else {
+            // EL BLOQUE TIENE LA REFERENCIA DE SUBTOTALES
+
+            leftEarns = arrayTextLine[end].arrayText[0]?.left;
+            leftDiscounts = arrayTextLine[end].arrayText[1]?.left;
+
+            jsonClient.devengos.subtotal =
+              arrayTextLine[end].arrayText[0]?.text.split("$ ")[1];
+            jsonClient.descuentos.subtotal =
+              arrayTextLine[end].arrayText[1]?.text.split("$ ")[1];
+          }
 
           /**
            * Recorrido del documento para capturar datos fijos donde se
@@ -148,6 +174,147 @@ const readPaymentgSupport = (filePath, isRequest = false) =>
            * en cuenta que cada bloque tiene distintas columnas. Además de que
            * 0 -> Es la primera ya que se devuelve como un Array
            */
+
+          // NOTA: LOS TEXTOS QUE NO TIENEN COHERENCIA EN LAS
+          // VALIDACIONES SON CASOS ESPECIFICOS DE DOCUMENTOS
+          // QUE NO SON BIEN LEIDOS
+          // guardar solo numeros .replace(/\D/g, "")
+          // guardar solo texto .replace(/[0-9]+/g, "")
+
+          /**
+           * sacamos todas las lineas leidas
+           */
+          for (let i = 0; i < arrayTextLine.length; i++) {
+            let bloque = i + 1;
+            let columna = 0;
+
+            arrayTextLine[i].arrayText.map((x) => {
+              // GUARDANDO CONVENIO ###################### ----------------------------------
+              if (x.text.startsWith("CENTRO DE UTILIDAD")) {
+                let codigoDividido = "";
+
+                // Si el bloque de abajo empieza con el codigo 0 viene el nombre del convenio
+                if (
+                  arrayTextLine[bloque].arrayText[columna]?.text.startsWith("0")
+                ) {
+                  codigoDividido =
+                    arrayTextLine[bloque].arrayText[columna]?.text.split(" ");
+                } else {
+                  // El bloque de abajo viene con otro texto o marca de agua
+                  codigoDividido =
+                    arrayTextLine[bloque + 1].arrayText[columna]?.text.split(
+                      " "
+                    );
+                }
+
+                let textConvenioSeparado = codigoDividido.slice(2);
+                let textConvenio = textConvenioSeparado.join(" ");
+
+                jsonClient.convenio = textConvenio;
+              }
+
+              // GUARDANDO NOMBRE, CEDULA, SALARIO BASE (DEPENDIENDO DE)
+              // LA LINEA ###################### ----------------------------------
+              if (x.text.includes("NOMBRE")) {
+                // Guardando Salario base dependiendo de la linea
+                let textOtroBloque = arrayTextLine[i + 4].arrayText[0]?.text;
+                if (textOtroBloque.includes("$")) {
+                  // Sueldo en la misma linea
+                  let obtenerSueldo = textOtroBloque.split("$ ")[1];
+                  let sueldo = obtenerSueldo.split(" ")[0];
+                  jsonClient.basico = sueldo;
+                } else {
+                  // Guardando sueldo base
+                  jsonClient.basico = x.text.split("$ ")[1];
+                }
+
+                // Guardando numero de cedula
+                let textInicioDocumento = x.text.split("(")[1];
+                let numDocumento = textInicioDocumento.split(")")[0];
+                jsonClient.documentNumber = numDocumento;
+
+                // Guardando nombre del cliente
+                let nameSeparado = x.text.split(": ")[2];
+                let name = nameSeparado.split(" (")[0];
+                jsonClient.name = name;
+              }
+
+              // GUARDANDO SUELDO NETO ###################### ----------------------------------
+              if (x.text.includes("NETO: ")) {
+                jsonClient.sueldoNeto = x.text.split("$ ")[1];
+              }
+
+              // GUARDANDO NUMERO DE CUENTA BANCARIA Y NOMBRE
+              // ###################### ----------------------------------
+              if (x.text.toUpperCase().startsWith("MODO PAGO:")) {
+                if (x.text.includes("<Ninguno>")) {
+                  jsonClient.banco.account = "NO REGISTRA";
+                  jsonClient.banco.name = "NO REGISTRA";
+                } else {
+                  jsonClient.banco.account = arrayTextLine[i].arrayText[
+                    columna
+                  ].text.replace(/\D/g, "");
+
+                  let separarCad = x.text.split(" ");
+                  let ubicacionBanco = separarCad.indexOf("BANCO");
+                  let name = separarCad.slice(ubicacionBanco);
+                  jsonClient.banco.name = name.join(" ");
+                }
+              }
+
+              // GUARDANDO FECHA DE NOMINA ###################### -------------------------------
+              if (x.text.includes("PERIODO:")) {
+                let capturaFecha = x.text.split(" ")[3];
+                let dividirFecha = capturaFecha.split("(")[1];
+                let fecha = dividirFecha.split(")")[0];
+                jsonClient.nomina = fecha;
+              }
+
+              // GUARDANDO NOMBRE DE EMPRESA Y NIT ###################### ------------------
+              if (x.text.toUpperCase().includes("EMPRESA:")) {
+                // Datos del nit
+                let dividirNit = "";
+                let nit = "";
+
+                // Datos de la empresa
+                let dividirEmpresa = x.text.split(": ")[1];
+                let name = dividirEmpresa.split("(")[0];
+
+                // Si el nombre de la empresa no captura numeros
+                if (isNaN(parseInt(x.text.replace(/\D/g, "")))) {
+                  // SOLO VIENE TEXTO
+                  jsonCompany.name = name;
+
+                  // se guarda el nit del bloque i + 4 (Caso especial)
+                  // Y si viene el sueldo base junto con el
+                  let textOtroBloque = arrayTextLine[i + 4].arrayText[0]?.text;
+                  if (textOtroBloque.includes("(")) {
+                    dividirNit = textOtroBloque.split("(")[1];
+                    nit = dividirNit.split(")")[0];
+                    jsonCompany.nit = nit;
+                  }
+                } else {
+                  // NOMBRE DE EMPRESA Y NIT EN LA MISMA LINEA
+                  jsonCompany.name = name;
+
+                  dividirNit = x.text.split("(")[1];
+                  nit = dividirNit.split(")")[0];
+
+                  // Si el nit incluye un guión
+                  if (x.text.includes("-")) {
+                    jsonCompany.nit = nit;
+                  } else {
+                    jsonCompany.nit = x.text.replace(/\D/g, "");
+                  }
+                }
+              }
+
+              // Calculo de puntuacion de confiabilidad de lectura del documento
+              contConfidence += x.confidence;
+              totalDatos++;
+              jsonClient.confidence = (contConfidence / totalDatos).toFixed(2);
+            });
+          }
 
           /**
            * Objeto que guarda los elementos que tengan devengos
@@ -178,17 +345,11 @@ const readPaymentgSupport = (filePath, isRequest = false) =>
            * arrayTextLine[i].arrayText[3] -> Columna Devengos o Descuento
            */
 
-          // ############################################
-          // console.log(
-          //   "JSON COMPANY ----------------------------------------------------------"
-          // );
-          // console.log(jsonCompany);
+          // for (let i = init + 1; i < end; i++) {
+          //   console.log(arrayTextLine[i]);
+          // }
 
-          // console.log(
-          //   "JSON CLIENT ----------------------------------------------------------"
-          // );
-          // console.log(jsonClient.devengos);
-
+          // AÑADIENDO LOS RESULTADOS DE LOS OBJETOS AL ARRAY
           resultArray.push(jsonClient);
           resultArray.push(jsonCompany);
           // console.log("JSON A EXPORTAR");
